663acdf3a56a1c060da343dc98eebf0365542dfe
Manu <manuelitoys93@gmail.com>
2023-05-26 18:05:45 -0500

    Informacion
    Configuracion de respuesta de informacion desde la API

--- controllers/items.ts
+++ controllers/items.ts
@@ -1,15 +1,53 @@
+import { itemsInterface } from "../interfaces/itemsInterface";
+
 const { response } = require('express');
+const axios = require('axios')
+
+const _apiMeli = 'https://api.mercadolibre.com/sites/MLA/search?q=';
+let _itemsInterface: itemsInterface = {} 
+const _itemsNew: any = []
+
 
 const items = (req: any, res: any = response) =>{
+    
+    axios.get(_apiMeli + req.body.search)
+    .then(({ data }: any) =>{
+            
+            let _items = data.results
+            _items.map((
+                { 
+                    id, title, 
+                    currency_id, 
+                    installments, 
+                    price, thumbnail, 
+                    condition, shipping
+                } : any
+            ) => {
+                _itemsInterface = {
+                    id: id,
+                    title: title,
+                    price: {
+                        currency: currency_id,
+                        amount: installments.amount,
+                        decimals: price
+                    },
+                    picture: thumbnail,
+                    condition: condition,
+                    free_shipping: shipping.free_shipping
+                }    
+                _itemsNew.push( _itemsInterface )
+            })
 
-    console.log(req.body);
-    
-    res.json({
-        status: 'ok',
-        msg: 'items',
-        user: req.body
-    })
-    
+
+            res.json({
+                author: {
+                    name: 'Manuel David',
+                    lastname: 'Rodriguez Riveros'
+                },
+                items: _itemsNew
+            })
+        })
+
 }
 
 module.exports = {
